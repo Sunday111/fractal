8357cd1baeb1cd8fecb5e9f52f5a1da3533deeb6
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2023-05-14 13:41:11 +0300

    Little optimization

--- gpu_fractal/code/mandelbrot.hpp
+++ gpu_fractal/code/mandelbrot.hpp
@@ -10,28 +10,20 @@ inline size_t MandelbrotLoop(const Float& x0, const Float& y0, size_t max_iterat
     Float y(0.0);
     size_t iteration = 0;
 
-    while ((x * x + y * y).compare(Float(4.0)) >= 0 && iteration != max_iterations)
+    const Float limit(4.0);
+    const Float two(2.0);
+
+    while (iteration != max_iterations)
     {
-        Float x_temp = x * x - y * y + x0;
-        y = Float(2) * x * y + y0;
-        x = x_temp;
-        ++iteration;
-    }
+        auto xs = x * x;
+        auto ys = y * y;
+        if ((xs + ys).compare(limit) > 0)
+        {
+            break;
+        }
 
-    return iteration;
-}
-
-template <typename T>
-inline size_t MandelbrotLoop(const T& x0, const T& y0, size_t max_iterations)
-{
-    T x(0.0);
-    T y(0.0);
-    size_t iteration = 0;
-
-    while ((x * x + y * y) < 4.0 && iteration != max_iterations)
-    {
-        T x_temp = x * x - y * y + x0;
-        y = T(2) * x * y + y0;
+        Float x_temp = xs - ys + x0;
+        y = two * x * y + y0;
         x = x_temp;
         ++iteration;
     }
